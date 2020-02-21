d977b68a4e1910738a7fab4ed70b8a1df4b596c9
paria03 <paria03@outlook.com>
2020-02-21 17:26:28 -0500

    new project structure

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const favicon = require('serve-favicon');
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
+
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => console.log(`Gator app listening on port ${port}`));
